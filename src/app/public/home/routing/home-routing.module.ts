541fa8a38b34bc52cd2e9fc8b3dbefad6b4d1ccd
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-07 13:44:23 -0500

    fix(home): Implementa routes para Home

--- src/app/public/home/routing/home-routing.module.ts
+++ src/app/public/home/routing/home-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../../login/login/login.component';
+import { HomeComponent } from '../home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo:'home',
+    pathMatch:'full'
+  },
+  {
+    component:HomeComponent,
+    title:"Home",
+    path:'home'
+  },
+  {
+    path:"login",
+    component: LoginComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
