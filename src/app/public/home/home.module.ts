7641b68764a3ca7442c960a0d5c0b7a4a7193cab
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-07 15:03:49 -0500

    fix(shared): Mueve componente header a module shared

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './routing/home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { HeaderHomeComponent } from './header-home/header-home.component';
 import { LoginModule } from '../login/login.module';
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    HeaderHomeComponent
+    HomeComponent
   ],
   imports: [
     CommonModule,
