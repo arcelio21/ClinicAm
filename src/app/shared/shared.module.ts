15dd0bc0ffe3cdb3aafbabfe43e4e6c41bef58db
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-18 11:39:17 -0500

    chore(shared): Modifica componentes de shared
    - Modifica component header que se utilizara en la app
    - Declara componentes creados en shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { ButtonComponent } from './button/button.component';
 import { ButtonRedirectComponent } from './button-redirect/button-redirect.component';
 import { RouterModule } from '@angular/router';
 import { HeaderHomeComponent } from './header-home/header-home.component';
+import { SectionFieldComponent } from './section-field/section-field.component';
 
 
 
@@ -11,12 +12,13 @@ import { HeaderHomeComponent } from './header-home/header-home.component';
   declarations: [
     ButtonComponent,
     ButtonRedirectComponent,
-    HeaderHomeComponent
+    HeaderHomeComponent,
+    SectionFieldComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
-  exports: [ButtonComponent,ButtonRedirectComponent,HeaderHomeComponent]
+  exports: [ButtonComponent,ButtonRedirectComponent,HeaderHomeComponent,SectionFieldComponent]
 })
 export class SharedModule { }
