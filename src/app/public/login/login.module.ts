d2654420db4f2c6c53fa544818484f6871f99e11
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-18 11:51:10 -0500

    feat(login): Importa componentes que se utilizan en login

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { FormLoginComponent } from './form-login/form-login.component';
+import { FormImgLoginComponent } from './form-img-login/form-img-login.component';
+import { SectionFieldComponent } from '../../shared/section-field/section-field.component';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    FormLoginComponent,
+    FormImgLoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class LoginModule { }
