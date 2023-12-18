2802ce213ee9055e1823f8d5ec094f07a802b9a8
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-18 11:36:17 -0500

    feat(login): Muestra imagen en layout de login

---
+++ src/app/public/login/form-img-login/form-img-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-img-login',
+  templateUrl: './form-img-login.component.html',
+  styleUrls: ['./form-img-login.component.css']
+})
+export class FormImgLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
