241df627bed0402dcb7cb0576b2dec0949143ba2
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-18 11:22:49 -0500

    chore(public): Modificaciones necesarias
    - Elimina etiqueta outlet innecesaria
    - Elimina importacion innecesaria
    - Agrega titulo a pagina de login

--- src/app/routing/app.routes.ts
+++ src/app/routing/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { HomeComponent } from "../public/home/home/home.component";
 import { HomeModule } from "../public/home/home.module";
 
 export const ROUTES_APP:Routes = [
