06264704c91453ecab27ecd0cec7578127db8d4f
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-20 11:27:11 -0500

    feat(login): Manipula inputs y estilos
    - Agrega controles para inputs del login
    - Agrega validadores para inputs
    - valida los errores de validadores
    - Modifica estilos de layout

--- src/app/public/login/form-login/form-login.component.ts
+++ src/app/public/login/form-login/form-login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.css']
+  styleUrls: ['./form-login.component.css'],
 })
 export class FormLoginComponent implements OnInit {
+  idenCard = new FormControl('', [Validators.required]);
+  password = new FormControl('', [
+    Validators.minLength(8),
+    Validators.required,
+  ]);
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  sendData() {
+    if (this.password.valid && this.idenCard.valid) {
+      console.log('ENVIO DE DATOS');
+    } 
   }
-
 }
