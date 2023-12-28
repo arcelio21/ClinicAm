8edd92012193b34219cd4d288525d4794129215c
Arcelio Montezuma <arceliomonte15@gmail.com>
2023-12-28 14:23:36 -0500

    feat(global): Define librerias que se utilizaran en modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './public/home/home.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,6 +17,10 @@ import { HomeModule } from './public/home/home.module';
     BrowserModule,
     HomeModule,
     AppRoutingModule,
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent]
