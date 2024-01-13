9ca66448daeea1103586ba1aef29a1adfc3e75fb
Arcelio Montezuma <arceliomonte15@gmail.com>
2024-01-13 13:08:51 -0500

    feat(shared): crea modelos para direcciones

---
+++ src/app/shared/model/ProvinceGet.ts
@@ -0,0 +1,5 @@
+import { Option } from "./Option";
+
+export interface ProvinceGet extends Option{
+
+}
---
+++ src/app/shared/model/VillageGet.ts
@@ -0,0 +1,5 @@
+import { Option } from "./Option";
+
+export interface VillageGet extends Option{
+  districtId: number;
+}
