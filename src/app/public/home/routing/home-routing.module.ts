8bc8a4ac7490eee6c3f6446046c1e77c54c6ebfd
Arcelio Montezuma <arceliomonte15@gmail.com>
2024-01-13 12:21:19 -0500

    chore(home): Agrega modulo register creado

--- src/app/public/home/routing/home-routing.module.ts
+++ src/app/public/home/routing/home-routing.module.ts
@@ -2,27 +2,33 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from '../../login/login/login.component';
 import { HomeComponent } from '../home/home.component';
+import { RegisterComponent } from '../../register/components/register/Register.component';
 
 const routes: Routes = [
   {
-    path:'',
-    redirectTo:'home',
-    pathMatch:'full'
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
   },
   {
-    component:HomeComponent,
-    title:"Home",
-    path:'home'
+    component: HomeComponent,
+    title: 'Home',
+    path: 'home',
   },
   {
-    path:"login",
+    path: 'login',
     component: LoginComponent,
-    title:"Login",
-  }
+    title: 'Login',
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    title: 'Register',
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class HomeRoutingModule { }
+export class HomeRoutingModule {}
